b7c6cc3eb875224abf9555ac7afa6a291d7229f5
Jornay <lucasdeafg@yahoo.com.br>
2023-06-21 14:56:20 -0300

    fix.component

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# AngularIntegration
+# AngularPipedriveBling
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.1.0.
 
--- src/app/data-list/data-list.component.spec.ts
+++ src/app/data-list/data-list.component.spec.ts
@@ -1,16 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { DataComponent } from './data.component';
+import { DataListComponent } from './data-list.component';
 
-describe('DataComponent', () => {
-  let component: DataComponent;
-  let fixture: ComponentFixture<DataComponent>;
+describe('DataListComponent', () => {
+  let component: DataListComponent;
+  let fixture: ComponentFixture<DataListComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [DataComponent]
+      declarations: [DataListComponent]
     });
-    fixture = TestBed.createComponent(DataComponent);
+    fixture = TestBed.createComponent(DataListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
