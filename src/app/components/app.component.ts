c27b1920bc50ee41d4f0ee45088ed2337767e9eb
Jornay <lucasdeafg@yahoo.com.br>
2023-06-23 19:49:45 -0300

    feat.pagination-function

--- src/app/components/app.component.ts
+++ src/app/components/app.component.ts
@@ -8,18 +8,39 @@ import { DealsService } from '../services/deals.service';
 })
 export class AppComponent {
   title = 'angular_pipedrive_bling';
-  dealsData: any = []
+  dealsData: any = [];
+  pagedDealsData: any = [];
+  currentPage = 1;
+  pageSize = 10;
+  index = 0
 
-  constructor(private dealsService: DealsService) {
-  }
+  constructor(private dealsService: DealsService) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
     this.getAllDeals();
+    this.updatePageData();
   }
+
 
   getAllDeals(): void {
     this.dealsService.getAll()
       .then(deals => this.dealsData = deals)
       .catch(error => console.error(error));
+  }
+
+  onPageChange(event: number) {
+    this.currentPage = event;
+    this.updatePageData();
+  }
+
+  getRelativeIndex(index: number): number {
+    const startIndex = (this.currentPage - 1) * this.pageSize;
+    return startIndex + index;
+  }
+
+  updatePageData() {
+    const startIndex = (this.currentPage - 1) * this.pageSize;
+    this.pagedDealsData = this.dealsData.slice(startIndex, startIndex + this.pageSize);
+   
   }
 }
